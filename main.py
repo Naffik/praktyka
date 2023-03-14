85841cb698d04121b48f02777fdb38f87755dfdd
Naffi <fakenaffi@gmail.com>
2023-03-14 17:39:03 +0100

    Added template class and function to add searched item to template object

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import json
-import pprint
-import tkinter
-import tkinter.messagebox
+import objectpath
 import customtkinter
-import xml.etree.ElementTree as ET
 import xmltodict
 
 
-def json_extract(obj: object, key: str) -> str:
+def json_extract(obj: object, key: str) -> dict:
     """Recursively fetch values from nested JSON."""
     arr = []
 
@@ -27,18 +24,26 @@ def json_extract(obj: object, key: str) -> str:
         return arr
 
     values = extract(obj, arr, key)
-    return values
+    return dict(values)
 
 
 class JSONTemplate:
+
     def __init__(self, template_file: str):
         with open(template_file, 'r') as file:
             self.template = json.load(file)
 
-    def add_data(self, data_file: str):
+    def add_data(self, data_file: str, search_key: str):
         with open(data_file, 'r') as file:
-            data = json.load(file)
-        self.template.update(data)
+            new_data = json.load(file)
+        # print(search_key)
+        # print(self.template)
+
+        if add_data_to_json(self.template, search_key, new_data):
+            print(f"Dodano nowe dane do klucza '{search_key}' w pliku '{self.template}'.")
+        else:
+            # jeśli wartość nie została znaleziona, wypisz stosowny komunikat
+            print(f"Nie znaleziono klucza '{search_key}' w pliku '{self.template}'.")
 
     def delete_data(self, key: str):
         if key in self.template:
@@ -53,11 +58,38 @@ class JSONTemplate:
             json.dump(self.template, file, indent=4)
 
 
+def add_data_to_json(json_data, key_to_find, new_data):
+    # print(json_data)
+    # json_data = json.dumps(json_data)
+    # print(json_data)
+    if isinstance(json_data, dict):
+        for k, v in json_data.items():
+            if k == key_to_find:
+                for key, value in new_data.items():
+                    # print(json_data[key_to_find][key])
+                    # json_data.pop(key_to_find)
+                    print(value)
+                    # json_data.update(value)
+                    json_data[key_to_find] = value
+                # json_data[k] = new_data
+                # print(json_data[k], end="\n")
+                return True
+            else:
+                if add_data_to_json(v, key_to_find, new_data):
+                    return True
+    elif isinstance(json_data, list):
+        for i, v in enumerate(json_data):
+            if add_data_to_json(v, key_to_find, new_data):
+                return True
+    return False
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         # configure window
+        self.key = None
         self.template = None
         self.data = None
         self.data_dict = None
@@ -83,36 +115,47 @@ class App(customtkinter.CTk):
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_1_label = customtkinter.CTkLabel(self.sidebar_frame, text="", anchor="w", width=180)
         self.sidebar_button_1_label.grid(row=2, column=0, padx=20, pady=(10, 0))
+
         self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Convert to JSON",
                                                         command=self.convert_to_json)
         self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10)
 
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Open JSON template file",
+                                                        command=self.load_template)
+        self.sidebar_button_3.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_3_label = customtkinter.CTkLabel(self.sidebar_frame, text="", anchor="w", width=180)
+        self.sidebar_button_3_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+
         self.sidebar_textbox_label = customtkinter.CTkLabel(self.sidebar_frame, text="Search",
                                                             font=customtkinter.CTkFont(size=16, weight="bold"))
-        self.sidebar_textbox_label.grid(row=4, column=0, padx=20, pady=10, sticky="sew")
+        self.sidebar_textbox_label.grid(row=6, column=0, padx=20, pady=10, sticky="sew")
         self.sidebar_textbox = customtkinter.CTkTextbox(self.sidebar_frame, width=160, height=60)
-        self.sidebar_textbox.grid(row=5, column=0, padx=20, pady=10, sticky="n")
+        self.sidebar_textbox.grid(row=7, column=0, padx=20, pady=10, sticky="n")
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Search",
                                                         command=self.search)
-        self.sidebar_button_3.grid(row=6, column=0, padx=20, pady=10)
+        self.sidebar_button_3.grid(row=8, column=0, padx=20, pady=10)
 
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, text="Save JSON file",
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, text="Add to template",
+                                                        command=self.add_to_template)
+
+        self.sidebar_button_4.grid(row=9, column=0, padx=20, pady=10)
+        self.sidebar_button_5 = customtkinter.CTkButton(self.sidebar_frame, text="Save JSON file",
                                                         command=self.save)
-        self.sidebar_button_3.grid(row=7, column=0, padx=20, pady=10)
+        self.sidebar_button_5.grid(row=10, column=0, padx=20, pady=10)
 
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=8, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=11, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
                                                                        values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=9, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=12, column=0, padx=20, pady=(10, 10))
 
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=10, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=13, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
                                                                values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=11, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=14, column=0, padx=20, pady=(10, 20))
 
         # create textbox
         self.textbox = customtkinter.CTkTextbox(self, width=250, height=680, activate_scrollbars=True)
@@ -147,18 +190,19 @@ class App(customtkinter.CTk):
     def convert_to_json(self):
         self.json_data = self.data_dict
         self.textbox.insert("0.0", json.dumps(self.data_dict, indent=4))
-        print(self.json_data)
+        # print(self.json_data)
 
     def search(self):
         self.textbox.delete("0.0", "end")
         with open("data.json", "r") as json_file:
             json_data = json.load(json_file)
-        key = self.sidebar_textbox.get("0.0", "end")
-        print(key.strip())
-        self.data = json_extract(json_data, key.strip())
+        self.key = self.sidebar_textbox.get("0.0", "end").strip()
+        # print(self.key.strip())
+        print(json_extract(json_data, self.key.strip()))
+        self.data = json_extract(json_data, self.key.strip())
         self.textbox.insert("0.0", json.dumps(self.data, indent=4))
-        # with open("data_search_1.json", "w") as outfile:
-        #     json.dump(data, outfile)
+        with open("search.json", "w") as outfile:
+            json.dump(self.data, outfile, indent=4)
 
     def save(self):
         filetypes = (('JSON files', '*.json'),)
@@ -169,8 +213,22 @@ class App(customtkinter.CTk):
             filetypes=filetypes,
             defaultextension="*.json")
         if filename:
-            with open(filename, "w") as file:
-                json.dump(self.json_data, file)
+            self.template.save_template(filename)
+
+    def load_template(self):
+        filetypes = (('JSON files', '*.json'),)
+
+        filename = customtkinter.filedialog.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+        f = filename.split("/")
+        self.sidebar_button_3_label.configure(text=f[-1])
+
+        self.template = JSONTemplate(filename)
+
+    def add_to_template(self):
+        self.template.add_data("search.json", self.key)
 
 
 if __name__ == "__main__":
